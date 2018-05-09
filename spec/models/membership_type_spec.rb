15a67eb9cb4d98eafb7d5028964d2f75789d801c
Mark Hermano <hermano.mark.mh@gmail.com>
2018-05-09 15:07:30 +0800

    membership types initial commit

---
+++ spec/models/membership_type_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe MembershipType, type: :model do
+
+  before :each do
+    @membershiptype = MembershipType.create do |mt|
+      mt.name = "Membership name" 
+      mt.price = 200
+      mt.total_price = 200
+      mt.status = true
+
+    end
+
+  end 
+
+  it "should be able to create a name" do
+    expect(@membershiptype.name).to eq "Membership name" 
+  end
+
+  it "should be able to create a description" do
+    expect(@membershiptype.price).to eq 200
+  end
+
+  it "should be able to create a price" do
+    expect(@membershiptype.total_price).to eq 200
+  end
+
+  it "should be able to create a facilities" do
+    expect(@membershiptype.status).to eq true
+  end
+
+end
