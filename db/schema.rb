5429cab6db0e675f31869ba6592cb6c023e9745d
Mark Hermano <hermano.mark.mh@gmail.com>
2018-05-09 15:07:25 +0800

    membership types initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509061635) do
+ActiveRecord::Schema.define(version: 20180509070456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "membership_types", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "price"
+    t.integer "total_price"
+    t.boolean "status"
+  end
 
   create_table "room_bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
