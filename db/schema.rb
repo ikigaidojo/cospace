b1b0c1c8d942c51a4e58308b1cda2dc06ae4388a
Your Name <you@example.com>
2018-05-09 15:50:30 +0800

    Added Resources.

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509061635) do
+ActiveRecord::Schema.define(version: 20180509073542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "room_bookings", force: :cascade do |t|
+  create_table "resources", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "room_bookings", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "price"
     t.string "facilities"
     t.string "location"
     t.datetime "date_booked"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "name"
     t.text "description"
     t.integer "price"
     t.string "facilities"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
