b4fde7a4add255d262356e377a0461e172ca01ee
Kate <kateyamson@gmail.com>
2018-05-10 11:10:01 +0800

    task-#30

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+
   before :each do
+
     @event = Event.create do |ev|
       ev.name = "Birthday" 
       ev.description = "meeting"
-      ev.room = "Center Room"
+    ev.attendees = 30  ev.room = "Center Room"
       ev.price = 5000
       ev.attendees = 30
     end
@@ -17,18 +19,18 @@ RSpec.describe Event, type: :model do
   end
 
   it "should be able to create a description" do
-    expect(@event.description).to eq "meeting" 
-  end
+     expect(@event.description).to eq "meeting" 
+   end
 
-  it "should be able to create a room" do
-    expect(@event.room).to eq "Center Room" 
-  end
+   it "should be able to create a room" do
+     expect(@event.room).to eq "Center Room" 
+   end
 
-  it "should be able to create a price" do
-    expect(@event.price).to eq 5000
-  end
+   it "should be able to create a price" do
+     expect(@event.price).to eq 5000
+   end
 
-  it "should be able to create a attendees" do
-    expect(@event.attendees).to eq 30
-  end
+   it "should be able to create a attendees" do
+     expect(@event.attendees).to eq 30
+   end
 end
