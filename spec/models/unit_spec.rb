d02a1b05099999abba9520a3464cd202ba2d22cf
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2018-05-10 17:52:25 +0800

    Fix spaces.

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe Unit, type: :model do
   
   before :each do
-	@label = "per hour"
-	@description = "per hour"
+	  @label = "per hour"
+	  @description = "per hour"
 	
-	@unit = Unit.create do |m|
-		m.label = @label
-		m.description = @description
-	end
+	  @unit = Unit.create do |m|
+		  m.label = @label
+		  m.description = @description
+	  end
   end
 
-    it "should be able to create a label attribute" do
-  		expect(@unit.label).to eq @label
-  	end
+  it "should be able to create a label attribute" do
+    expect(@unit.label).to eq @label
+  end
 
-  	it "should be able to create a description attribute" do
-  		expect(@unit.description).to eq @description
-  	end
+  it "should be able to create a description attribute" do
+    expect(@unit.description).to eq @description
+  end
 end
