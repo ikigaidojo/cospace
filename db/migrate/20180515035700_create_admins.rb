143eb13b6f657a88187d0f35caa4d569562f9b33
Your Name <you@example.com>
2018-05-15 12:45:31 +0800

    Added Admin Model.

---
+++ db/migrate/20180515035700_create_admins.rb
@@ -0,0 +1,11 @@
+class CreateAdmins < ActiveRecord::Migration[5.1]
+  def change
+    create_table :admins do |t|
+
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.timestamps
+    end
+  end
+end
