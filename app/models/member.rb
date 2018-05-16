0f00e985ad469f5fa3959409686fba69a94e7957
Your Name <you@example.com>
2018-05-16 14:56:13 +0800

    Added and Deleted in member.rb.

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,10 +5,8 @@ class Member < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
 
   validates_presence_of       :first_name,      :message => "Can't be blank"
-  validates_presence_of       :last_name,       :message => "Can't be blank"
   validates_presence_of       :email,           :message => "Can't be blank"
-  validates :email, uniqueness: true
-  validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, :message => "Must be a valid email"
+  validates_format_of         :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, :message => "Must be a valid email"
   validates_presence_of       :phone_number,    :message => "Can't be blank", :allow_nil => true
   validates_presence_of       :gender,          :message => "Can't be blank", :allow_nil => true
   validates_presence_of       :password,        :message => "Can't be blank"
