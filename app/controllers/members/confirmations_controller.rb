d5e379486e09289ce8375c6d0d351125ef468b4b
Mark Hermano <hermano.mark.mh@gmail.com>
2018-05-16 11:59:51 +0800

    fix uninitialize error for sign up and made some changes on navbar when user logs in

--- app/controllers/members/confirmations_controller.rb
+++ app/controllers/members/confirmations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Member::ConfirmationsController < Devise::ConfirmationsController
+class Members::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation/new
   # def new
   #   super
