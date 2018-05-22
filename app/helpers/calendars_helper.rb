1e1a08d685175b6eb1abaf2f7fc56bc94fd5fba0
Finn <finn.rothacker@gmail.com>
2018-05-22 15:13:55 +0800

    description and name and price are called and and updated in deescription, crappy fix to get the prices working was made, however have not yet installed money gem

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -6,7 +6,7 @@ module CalendarsHelper
     Room.all.each do |room|
       room_booked = room.room_bookings.where(date_booked: DateTime.parse(date))
       if room_booked.empty? 
-        spare_rooms << room.name
+        spare_rooms << [room.name, room.description, room.price]
       end
     end
 
@@ -16,58 +16,61 @@ module CalendarsHelper
               <div class="row">'
 
 
-######################## ----- list of rooms  ----- ########################
+        ######################## ----- list of rooms  ----- ########################
         # adds div classes for the LIST of rooms
-        html.concat('<div class="col-4">
-                      <div class="list-group" id="list-tab" role="tablist">')
+        html.concat("<div class='col-4'>
+                      <div class='list-group' id='list-tab' role='tablist'>
+                        <p> available on #{date}</p>")
+
         
-        # create list of room names. 
-        spare_rooms.each do |room_name|
-          html.concat(
-            '<a 
-              class = "list-group-item list-group-item-action" 
-              id="list-' +room_name+ '-list" 
-              data-toggle="list" href="#list-' +room_name+ '" role="tab" 
-              aria-controls="' +room_name+ '">' +room_name+  
-            '</a>')
-        end
-        # end divs for list divs
-        html.concat('</div> </div>')
+      # create list of room names. 
+      spare_rooms.each do |room|
+        room_name = room[0]
+        description = room[1]
+        price = room[2]
+
+        html.concat(
+          "<a 
+            class = 'list-group-item list-group-item-action' 
+            id='list-#{ room_name }-list' 
+            data-toggle='list' href='#list-#{ room_name }' role='tab' 
+            aria-controls='#{ room_name }'> #{ room_name }  
+          </a>")
+      end
+      # end divs for list divs
+      html.concat("</div> </div>")
 
 
-######################## ----- descriptions  ----- ########################
-        # create divs for descriptoins
-        html.concat('<div class="col-8">
-                      <div class="tab-content" id="nav-tabContent">')
-        
-        # create descriptions of rooms. 
-        spare_rooms.each do |room_name|
-          html.concat('<div 
-              class="tab-pane fade " 
-              id="list-'+room_name+'" 
-              role="tabpanel" aria-labelledby="list-'+room_name+'-list"> 
-              <h1>' +room_name+' </h1>
-              <p>' +room_name+' is a cozy room nested in the back of launchgarage  
-              <br>
-              It was named after the starwars planet, '+room_name+'
-              <br></p>
-              
-            </div>') 
-        end
+      ######################## ----- descriptions  ----- ########################
+      # create divs for descriptoins
+      html.concat("<div class='col-8'>
+                    <div class='tab-content' id='nav-tabContent'>")
+      
+      # create descriptions of rooms. 
+      spare_rooms.each do |room|
+        room_name = room[0]
+        description = room[1]
+        price = ('%.2f' % (room[2].to_i/100.0)) # or install monery gem
 
-        # # some placeholder content for descrption
-        # html.concat('<p> 
-        #   this is the description this is the description 
-        #   this is the description this is the description 
-        #   this is the description this is the description 
-        #   </p>')
+        html.concat("<div 
+                      class='tab-pane fade' 
+                      id='list-#{ room_name }' 
+                      role='tabpanel' aria-labelledby='list-#{ room_name }-list'> 
+                      <h1> #{ room_name } </h1>
+                      <br>
+                      Description: #{ description }
+                      <br>
+                      Price: $#{ price } 
+                      <br></p>              
+                    </div>") 
+      end
 
-        #end divs for descriptions
-        html.concat('</div> </div>')
+      #end divs for descriptions
+      html.concat('</div> </div>')
 
 
     # end divs for "available_rooms" and "row"
-    html.concat('</div></div>') 
+    html.concat('</div> </div>') 
 
     return html.html_safe 
   end #function end
