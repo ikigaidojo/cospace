833b9c543c811743292858534ab42d787fda19fa
Kate <kateyamson@gmail.com>
2018-05-29 17:23:13 +0800

    WIP:Update Password page

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -9,9 +9,11 @@ class SettingsController < ApplicationController
     @member = current_member
   end
 
+
+
   def update_password
     @member = current_member
-    if @member.update(member_params)
+    if @member.update_with_password(member_params)
       # Sign in the user by passing validation in case their password changed
       bypass_sign_in(@member)
       redirect_to root_path
@@ -19,11 +21,11 @@ class SettingsController < ApplicationController
       render "index"
     end
   end
- 
+
  private
 
   def member_params
     # NOTE: Using `strong_parameters` gem
-    params.require(:member).permit(:password, :password_confirmation)
+    params.require(:member).permit(:current_password, :password, :password_confirmation)
   end
 end
