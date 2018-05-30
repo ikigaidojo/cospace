c1292c76ccc20910b7eb2559e3e1bd41085d2b2e
Finn <finn.rothacker@gmail.com>
2018-05-30 12:55:17 +0800

    info is now passed into the pdf generator

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,10 +7,9 @@ class InvoicesController < ApplicationController
     RoomBooking.all.each do |booking|
       if booking.member_id == current_member.id
         @bookings << booking
-        logger.info "booking number #{booking.id} added to @bookings"
-        @bookings = @bookings.reverse
       end
     end
+    @bookings = @bookings.reverse
   end
 
   def book_room
@@ -20,7 +19,6 @@ class InvoicesController < ApplicationController
     room = Room.where(id: room_id).first
 
     unless room == nil 
-
       new_room_booking = RoomBooking.create(
         name:        room.name, 
         description: room.description, 
@@ -33,8 +31,10 @@ class InvoicesController < ApplicationController
 
       if new_room_booking.persisted?
         room.room_bookings << new_room_booking
-        redirect_to invoices_index_path, notice: "Room booking successfull!"
-      end
+        redirect_to invoices_index_path, notice: "#{new_room_booking.name} booked successfully!"
+      else 
+        redirect_to :back, notice: "#{new_room_booking.name} booking failed"
+      end 
 
     else
       logger.info "room does not exist"
@@ -43,6 +43,9 @@ class InvoicesController < ApplicationController
 
   # this runs when 'invoice' button is clicked
   def create_pdf
+  @booked = RoomBooking.where(id: params[:id]).first
+  logger.info @booked
+
     respond_to do |format|
       format.html
       format.pdf do
@@ -51,9 +54,8 @@ class InvoicesController < ApplicationController
         layout: "pdf.html"
       end
     end
+   
   end
-
-
 end
 
 
