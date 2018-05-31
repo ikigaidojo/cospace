098ce127757298f782a1fdbf3dfe3ff2048e8860
Mark Hermano <hermano.mark.mh@gmail.com>
2018-05-31 14:49:13 +0800

    settings update

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -21,8 +21,38 @@ class Members::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   # def update
-  #   super
+  #  super
   # end
+
+  # GET /resource/edit
+  def edit
+    render :edit
+  end
+
+  # PUT /resource
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      bypass_sign_in resource, scope: resource_name
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with redirect_to: settings_path(resource)
+    end
+  end
+
 
   # DELETE /resource
   # def destroy
@@ -59,4 +89,10 @@ class Members::RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     '/pages/check_your_email'
   end
+
+  protected
+
+    def after_update_path_for(resource)
+      settings_path(resource)
+    end
 end
