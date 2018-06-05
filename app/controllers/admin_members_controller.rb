fd42a411a7ab80f1270dd8aa842bb21150f4f5be
Finn <finn.rothacker@gmail.com>
2018-06-05 17:17:53 +0800

    prevented non -admin users from viewing members list + added sidebar

--- app/controllers/admin_members_controller.rb
+++ app/controllers/admin_members_controller.rb
@@ -1,9 +1,15 @@
 class AdminMembersController < ApplicationController
   before_action :authenticate_member!
+  before_action :authenticate
+
+  def authenticate
+    unless current_member.is_admin == true
+      redirect_to calendar_calendars_path, notice: "admin page not permitted"
+    end
+  end
   
   def index
     @members = Member.all
-    
   end
 
 end
